1ae816fff4b924dfb00fc32fc30974751f38ef0b
ZineddineBk09 <eino1209@gmail.com>
2023-11-13 08:28:12 +0100

    use SessionProvider instead of AuthContextProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,17 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react'
 import { Layout } from '../components/layout/layout'
-import { AuthContextProvider } from '@/context/AuthContext'
 import { useRouter } from 'next/router'
 import CheckAuthGuard from '@/components/guards/CheckAuthGuard'
 import { MapContextProvider } from '@/context/MapContext'
+import { SessionProvider } from 'next-auth/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const path = useRouter().pathname.split('/')[1]
+  const { session }: any = pageProps
 
   return (
-    <AuthContextProvider>
+    <SessionProvider session={session}>
       <CheckAuthGuard>
         <NextUIProvider>
           {path === '' || path === 'register' ? (
@@ -29,7 +30,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           )}
         </NextUIProvider>
       </CheckAuthGuard>
-    </AuthContextProvider>
+    </SessionProvider>
   )
 }
 
