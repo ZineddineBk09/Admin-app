22b9bcfb5812df78c4bf3549253f915495f34726
ZineddineBk09 <eino1209@gmail.com>
2023-11-17 10:54:17 +0100

    add the function that get's the drivers in specefic BBOX

--- lib/api/map.ts
+++ lib/api/map.ts
@@ -1,3 +1,31 @@
-const getDriversInArea = async (req: Request, res: Response) => {
-  
+import { BBox } from '@/interfaces'
+
+export const getDriversInArea = async (bbox: BBox) => {
+  const drivers: any[] = []
+
+  // get drivers in bbox from websocket server: NEXT_PUBLIC_WEBSOCKET_URL/map
+  const mapSocket = new WebSocket(
+    process.env.NEXT_PUBLIC_WEBSOCKET_URL + '/map'
+  )
+
+  mapSocket.onopen = () => {
+    mapSocket.send(JSON.stringify(bbox))
+    console.log('map socket opened')
+  }
+
+  mapSocket.onmessage = (event) => {
+    const data = JSON.parse(event.data)
+    console.log('data: ', data)
+    drivers.push(data)
+  }
+
+  mapSocket.onclose = () => {
+    console.log('map socket closed')
+  }
+
+  mapSocket.onerror = (error) => {
+    console.log('map socket error: ', error)
+  }
+
+  return drivers
 }
