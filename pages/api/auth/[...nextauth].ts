7f6f9413b582a6838deb8638b70f47f1abd0c4cd
ZineddineBk09 <eino1209@gmail.com>
2023-11-17 11:48:28 +0100

    fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,19 +2,62 @@ import axios from 'axios'
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
+async function refreshAccessToken(token: any) {
+  console.log('Refreshing access token')
+  try {
+    const url = process.env.NEXT_PUBLIC_API_URL + '/refresh'
+
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        refreshToken: token.refreshToken,
+      }),
+    })
+
+    const refreshedTokens = await response.json()
+
+    if (!response.ok) {
+      throw refreshedTokens
+    }
+
+    console.log('Refreshed access token')
+    console.log(refreshedTokens)
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.data.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens.data.expires,
+      refreshToken: refreshedTokens.data.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+    }
+  } catch (error) {
+    console.log(error)
+
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    }
+  }
+}
+
 export default NextAuth({
   providers: [
     CredentialsProvider({
       name: 'fleetrun-auth',
       id: 'fleetrun-auth',
+      credentials: {
+        username: {
+          type: 'text',
+        },
+        password: {
+          type: 'password',
+        },
+      },
 
-      async authorize({
-        username,
-        password,
-      }: {
-        username: string
-        password: string
-      }) {
+      async authorize(credentials: { username: string; password: string }) {
+        const { username, password } = credentials
         try {
           const response = await axios.post(
             process.env.NEXT_PUBLIC_API_URL + '/token',
