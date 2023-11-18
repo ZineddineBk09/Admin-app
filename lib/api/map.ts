10fba3b2e585350445d67004d484d254a62db4a3
ZineddineBk09 <eino1209@gmail.com>
2023-11-18 09:14:13 +0100

    fix

--- lib/api/map.ts
+++ lib/api/map.ts
@@ -4,28 +4,71 @@ export const getDriversInArea = async (bbox: BBox) => {
   const drivers: any[] = []
 
   // get drivers in bbox from websocket server: NEXT_PUBLIC_WEBSOCKET_URL/map
-  const mapSocket = new WebSocket(
+  const mapSocket: any = new WebSocket(
     process.env.NEXT_PUBLIC_WEBSOCKET_URL + '/map'
   )
 
   mapSocket.onopen = () => {
-    mapSocket.send(JSON.stringify(bbox))
-    console.log('map socket opened')
+    if (mapSocket.readyState === WebSocket.OPEN) {
+      mapSocket.send(
+        JSON.stringify({
+          min_lat: 37.7,
+          min_lng: 120.5,
+          max_lat: 40.8,
+          max_lng: 122.23,
+        })
+      )
+
+      //  console.log('map socket opened')
+    } else {
+      console.log('map socket not opened')
+    }
   }
 
-  mapSocket.onmessage = (event) => {
+  mapSocket.onmessage = (event: MessageEvent) => {
+    console.log('map socket message: ', event)
     const data = JSON.parse(event.data)
-    console.log('data: ', data)
-    drivers.push(data)
+    console.log('map data: ', data)
+    return data
   }
 
   mapSocket.onclose = () => {
-    console.log('map socket closed')
+    //console.log('map socket closed')
   }
 
-  mapSocket.onerror = (error) => {
+  mapSocket.onerror = (error: any) => {
     console.log('map socket error: ', error)
+    mapSocket.close()
   }
 
   return drivers
+}
+
+export const addDriverToArea = async (bbox: BBox) => {
+  // get a random (lat, lng) using max_lat, min_lat, max_lng, min_lng
+  const lat: number = bbox.min_lat
+  const lng: number = bbox.min_lng
+
+  // send (lat, lng) to websocket server: NEXT_PUBLIC_WEBSOCKET_URL/driver
+  const driverSocket = new WebSocket(
+    process.env.NEXT_PUBLIC_WEBSOCKET_URL + '/driver'
+  )
+
+  driverSocket.onopen = () => {
+    driverSocket.send(JSON.stringify({ lat, lng }))
+    console.log('driver socket opened')
+  }
+
+  driverSocket.onmessage = (event: MessageEvent) => {
+    const data = JSON.parse(event.data)
+    console.log('driver data: ', data)
+  }
+
+  driverSocket.onclose = () => {
+    console.log('driver socket closed')
+  }
+
+  driverSocket.onerror = (error: any) => {
+    console.log('driver socket error: ', error)
+  }
 }
