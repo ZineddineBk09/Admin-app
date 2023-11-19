aad1d9048803570c014dd18d6fcf489eed11c7ac
ZineddineBk09 <eino1209@gmail.com>
2023-11-19 10:37:06 +0100

    some basic API CRUD functions

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -4,15 +4,80 @@ import { getSession } from 'next-auth/react'
 export const createRecord = async (record: APIRecord, endpoint: string) => {
   const session: any = await getSession()
   console.log('createRecord session: ', session)
-  const response = await fetch(process.env.NEXT_PUBLIC_API_URL + endpoint, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${session.accessToken}`,
-    },
-    body: JSON.stringify(record),
-  })
+
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + '/' + endpoint,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${session.accessToken}`,
+      },
+      body: JSON.stringify(record),
+    }
+  )
   const data = await response.json()
   console.log('createRecord data: ', data)
   return data
+}
+
+export const updateRecord = async (record: APIRecord, endpoint: string) => {
+  const session: any = await getSession()
+  console.log('updateRecord session: ', session)
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + '/' + endpoint,
+    {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${session.accessToken}`,
+      },
+      body: JSON.stringify(record),
+    }
+  )
+  const data = await response.json()
+  console.log('updateRecord data: ', data)
+  return data
+}
+
+export const deleteRecord = async (record: APIRecord, endpoint: string) => {
+  const session: any = await getSession()
+  console.log('deleteRecord session: ', session)
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + '/' + endpoint,
+    {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${session.accessToken}`,
+      },
+      body: JSON.stringify(record),
+    }
+  )
+  const data = await response.json()
+  console.log('deleteRecord data: ', data)
+  return data
+}
+
+export const getRecords = async (endpoint: string) => {
+  const session: any = await getSession()
+
+  try {
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_API_URL + '/' + endpoint,
+      {
+        method: 'GET',        
+        headers: {
+          'Content-Type': 'application/json',
+          // Authorization: `Bearer ${session.accessToken}`,
+        },
+      }
+    )
+    const data = await response.json()
+    console.log('getRecords data: ', data)
+    return data.teams
+  } catch (error) {
+    console.log('getRecords error: ', error)
+    return []
+  }
 }
