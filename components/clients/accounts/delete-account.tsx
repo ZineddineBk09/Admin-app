19eb38663838a6c41813c9f4eb0a2718de40980c
ZineddineBk09 <eino1209@gmail.com>
2023-11-21 08:18:57 +0100

    change loading spinner color

--- components/clients/accounts/delete-account.tsx
+++ components/clients/accounts/delete-account.tsx
@@ -49,7 +49,7 @@ export const DeleteAccount = ({ id }: { id: string }) => {
           </Text>
         </Modal.Header>
         {loading ? (
-          <Loading size='xl' className='my-3' color='success' />
+          <Loading size='xl' className='my-3' color='warning' />
         ) : (
           <Modal.Body>
             <Flex
