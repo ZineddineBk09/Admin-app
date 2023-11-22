7d094dbb206e7f0828c469d518efb17d6e2afd0d
ZineddineBk09 <eino1209@gmail.com>
2023-11-22 20:01:16 +0100

    fix

--- components/shared/Loading.tsx
+++ components/shared/Loading.tsx
@@ -5,7 +5,7 @@ export default function Loading() {
     <div className='z-10 fixed inset-0 h-screen w-screen bg-white/80 flex items-center justify-center'>
       <Image
         src={'/images/logo.png'}
-        alt={'Delivery App Logo'}
+        alt={'FleetRun Logo'}
         width={300}
         height={200}
         className='z-100 animate-pulse object-contain border border-white rounded-full h-40 w-40 p-4'
