9c5532f3136097ef6b5f02b923ead3826d8de4b8
ZineddineBk09 <eino1209@gmail.com>
2023-11-25 21:29:35 +0100

    orders page

--- pages/orders/index.tsx
+++ pages/orders/index.tsx
@@ -1,8 +1,14 @@
+import { OrdersPage } from '@/components/orders'
+import { OrdersContextProvider } from '@/context/order/OrdersContext'
 import { NextPage } from 'next'
 import React from 'react'
 
 const Orders: NextPage = () => {
-  return <div></div>
+  return (
+    <OrdersContextProvider>
+      <OrdersPage />
+    </OrdersContextProvider>
+  )
 }
 
 export default Orders
