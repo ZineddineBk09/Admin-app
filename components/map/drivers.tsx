9372caef39f5a9dde9eac5bf0433c522b9b3237d
ZineddineBk09 <eino1209@gmail.com>
2023-11-27 11:49:37 +0100

    remove fetching data logic

--- components/map/drivers.tsx
+++ components/map/drivers.tsx
@@ -5,127 +5,94 @@ import { faker } from '@faker-js/faker'
 import { DriverInboxIcon, DriverOrdersIcon } from '../icons/drivers'
 import { truncateTxt } from '@/utils'
 import { Checkbox, Tooltip } from '@nextui-org/react'
-import { useMapContext } from '@/context/MapContext'
+import { useMapContext } from '@/context/map/MapContext'
 
 const DriversList = () => {
-  const [driverStatus, setDriverStatus] = useState<Status[]>([
-    { value: 'Available', checked: true },
-    { value: 'Busy', checked: true },
-    { value: 'Inactive', checked: true },
-  ])
-  const [drivers, setDrivers] = useState<any[]>([] as any[])
-  const { showDrivers } = useMapContext()
-
-  const fetchDrivers = () => {
-    const arr = []
-    for (let i = 0; i < 20; i++) {
-      const fakeDriver: any = {
-        id: faker.string.uuid(),
-        username: faker.person.fullName(),
-        email: faker.internet.email(),
-        team: faker.company.name(),
-        completedTasks: faker.number.int({ max: 100, min: 0 }),
-        inProgressTasks: faker.number.int({ max: 100, min: 0 }),
-        image: faker.image.avatar(),
-        status: driverStatus[faker.number.int({ max: 2, min: 0 })].value as any,
-        location: {
-          latitude: faker.location.latitude({ max: 22, min: 21 }),
-          longitude: faker.location.longitude({
-            max: 40,
-            min: 39,
-          }),
-        },
-        orders: faker.number.int({
-          max: 10,
-          min: 0,
-        }),
-        phone: faker.phone.number(),
-      }
-      arr.push(fakeDriver)
-    }
-    setDrivers(arr as Driver[])
-  }
+  const { drivers, selectedDriver, showDrivers } = useMapContext()
+  const [driver, setDriver] = useState<any>(null)
 
   useEffect(() => {
-    fetchDrivers()
-  }, [])
-
-  const numberOfDrivers = (status: string) => {
-    return drivers.filter((driver) => driver.status === status).length
-  }
+    if (selectedDriver) {
+      const driver = drivers.find((d: any) => d.id === selectedDriver)
+      setDriver(driver)
+    } else {
+      setDriver(drivers[0])
+    }
+  }, [selectedDriver])
 
   if (!showDrivers) return null
   return (
-    <div className='w-[100%] flex flex-col mx-auto h-fit relative pl-2'>
-      {/*  Title & drivers states */}
-      <div className='w-full flex items-center justify-between z-[100]'>
-        <h1 className='font-semibold text-2xl'>Drivers</h1>
-        <div className='flex items-center gap-x-2'>
-          {driverStatus.map(({ value, checked }, i) => (
-            <div
-              key={i}
-              className='flex items-center gap-x-1 px-2 py-1 bg-white shadow-md rounded-full text-xs font-medium'
-            >
-              {value}{' '}
-              <div className='flex items-center gap-x-1 ml-4'>
-                <span className='text-xs w-5 h-5 text-center p-[2px] bg-gray-200 rounded-full'>
-                  {numberOfDrivers(value)}
-                </span>
-                <Checkbox
-                  aria-label='Checkbox-driver-status'
-                  color='warning'
-                  labelColor='warning'
-                  defaultSelected
-                  isRounded
-                  value={value}
-                  size='md'
-                  onChange={(checked) => {
-                    const newArr = [...driverStatus]
-                    newArr[i].checked = checked
-                    setDriverStatus(newArr)
-                  }}
-                ></Checkbox>
-              </div>
-            </div>
-          ))}
+    <div className='w-[80%] flex items-start justify-evenly mx-auto h-fit relative p-4'>
+      {/* Image & Status */}
+      <div className='flex flex-col items-center gap-y-3'>
+        <Image
+          src={driver?.image}
+          alt={driver?.username}
+          width={120}
+          height={120}
+          className='rounded-full'
+        />
+        <span
+          className={`py-2 px-6 rounded-full font-medium ${
+            driver?.status === 'Available'
+              ? 'bg-green-400'
+              : driver?.status === 'Busy'
+              ? 'bg-orange-500'
+              : 'bg-gray-400'
+          }`}
+        >
+          {driver?.status}
+        </span>
+      </div>
+
+      {/* Personal Infos */}
+      <div className='flex flex-col gap-y-2 ml-4 items-start'>
+        <div className='flex flex-col items-end'>
+          <p className='text-xl text-gray-600 font-medium'>
+            {driver?.username}
+          </p>
+          <span className='text-sm'>#{driver?.id}</span>
+        </div>
+
+        <p className='text-[#59AFFF] font-medium my-3'>
+          <span className='mr-2'>call:</span>
+          {driver?.phone}
+        </p>
+        <div className='w-full flex items-center justify-between gap-x-3'>
+          <span className='text-sm text-gray-500 font-medium'>
+            Delivered Orders
+          </span>
+          <span className='text-md font-medium'>{driver?.completedTasks}</span>
+        </div>
+        <div className='w-full flex items-center justify-between gap-x-3'>
+          <span className='text-sm text-gray-500 font-medium'>
+            Canceled Orders
+          </span>
+          <span className='text-md font-medium'>{driver?.inProgressTasks}</span>
+        </div>
+        <div className='w-full  flex items-center justify-between gap-x-3'>
+          <span className='text-sm text-gray-500 font-medium'>Warnings</span>
+          <span className='text-md font-medium'>{driver?.warnings}</span>
+        </div>
+        <div className='w-full  flex items-center justify-between gap-x-3'>
+          <span className='text-sm text-gray-500 font-medium'>
+            Active Hours
+          </span>
+          <span className='text-md font-medium'>{driver?.activeHours}</span>
         </div>
       </div>
 
-      {/* Drivers */}
-      <div>
-        {/* List 01 */}
-        <div className='overflow-x-scroll'>
-          <div className={`w-[98%] mx-auto grid grid-flow-col gap-x-4 my-4`}>
-            {drivers
-              .slice(0, drivers.length / 2)
-              .filter((dr) =>
-                driverStatus.some(
-                  (status) => status.value === dr.status && status.checked
-                )
-              )
-              .map((driver) => (
-                <DriverCard key={driver.id} driver={driver} />
-              ))}
-          </div>
-
-          {/* List 02 */}
-          <div className={`w-[98%] mx-auto grid grid-flow-col gap-x-4 my-4`}>
-            {drivers
-              .slice(drivers.length / 2, drivers.length)
-              .filter((dr) =>
-                driverStatus.some(
-                  (status) => status.value === dr.status && status.checked
-                )
-              )
-              .map((driver) => (
-                <DriverCard key={driver.id} driver={driver} />
-              ))}
-          </div>
-        </div>
-        {/* right white shadow */}
-        {drivers.length > 0 && (
-          <div className='w-40 absolute top-0 bottom-4 right-0 bg-gradient-to-l from-white  rounded-md rounded-tr-lg p-2 gap-y-4 ml-20 lg:bottom-0' />
-        )}
+      {/* Buttons */}
+      <div className='h-full flex flex-col justify-center gap-y-5'>
+        <button className='h-11 px-12 bg-gray-400 rounded font-medium text-lg shadow-lg hover:bg-opacity-90 transition-all duration-300'>
+          Attach Order
+        </button>
+        <button className='h-11 px-12 bg-gray-400 rounded font-medium text-lg shadow-lg hover:bg-opacity-90 transition-all duration-300'>
+          Warning
+        </button>
+        <button className='h-11 px-12 bg-red-500 rounded font-medium text-lg shadow-lg hover:bg-opacity-90 transition-all duration-300'>
+          Inactivate
+        </button>
       </div>
     </div>
   )
