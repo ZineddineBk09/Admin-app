0204bf74c53559b22dec9f2f574a1f6c58eb288f
ZineddineBk09 <eino1209@gmail.com>
2023-12-02 13:38:00 +0100

    add reports pqge ctx

--- pages/reports/index.tsx
+++ pages/reports/index.tsx
@@ -1,9 +1,14 @@
 import { ReportsPage } from '@/components/reports'
+import { ReportsContextProvider } from '@/context/report/ReportsContext'
 import { NextPage } from 'next'
 import React from 'react'
 
 const Reports: NextPage = () => {
-  return <ReportsPage />
+  return (
+    <ReportsContextProvider>
+      <ReportsPage />
+    </ReportsContextProvider>
+  )
 }
 
 export default Reports
