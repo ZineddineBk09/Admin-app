0e99fe5550011b16e6d419af717eba70b853d66e
ZineddineBk09 <eino1209@gmail.com>
2023-12-03 16:08:14 +0100

    add export to excel button

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,27 @@
+import exportFromJSON from 'export-from-json'
+
 export const truncateTxt = (str: string, n: number) => {
   return str.length > n ? str.substr(0, n - 1) + '...' : str
+}
+
+export const exportToExcel = ({ name, data }: { name: string; data: any }) => {
+  exportFromJSON({
+    data,
+    fileName: name,
+    exportType: 'xls',
+    fields: {
+      id: 'ID',
+      client: 'Client',
+      clientName: 'Client Name',
+      clientPhone: 'Client Phone',
+      clientEmail: 'Client Email',
+      paid: 'Paid',
+      unpaid: 'Unpaid',
+      branches: 'Branches',
+      areas: 'Areas',
+      countries: 'Countries',
+      orders: 'Orders',
+      canceledOrders: 'Canceled Orders',
+    },
+  })
 }
