8cf21411b36ccba0ab1f4892d9f206cb577848ec
ZineddineBk09 <eino1209@gmail.com>
2023-12-04 08:50:03 +0100

    support page context

--- components/support/index.tsx
+++ components/support/index.tsx
@@ -1,83 +1,11 @@
+import { useSupportContext } from '@/context/support/support-context'
 import React, { useEffect } from 'react'
 
 const SupportPage = () => {
   const el = document.getElementById('messages')
   // el?.scrollTop = el?.scrollHeight
   const messagesRef: any = React.useRef(null)
-  const support = [
-    {
-      id: 1,
-      text: "Hi there! I am wondering if you can help me with a problem I've been having.",
-    },
-    {
-      id: 2,
-      text: "Oh, hey. I'm not sure, but I'll do my best.",
-    },
-    {
-      id: 3,
-      text: "I've been trying to install this package but it's not working and I don't know why.",
-    },
-    {
-      id: 4,
-      text: 'What package are you trying to install?',
-    },
-    {
-      id: 5,
-      text: "It's called @vue/devtools",
-    },
-    {
-      id: 6,
-      text: "Oh, I think I know what's going on. Give me a second and I'll get back to you.",
-    },
-    {
-      id: 7,
-      text: 'Okay, thank you!',
-    },
-    {
-      id: 8,
-      text: "Okay, I'm back. Are you using npm or yarn?",
-    },
-    {
-      id: 9,
-      text: "I'm using npm.",
-    },
-    {
-      id: 10,
-      text: 'Okay, try running this command: npm install -g @vue/devtools',
-    },
-    {
-      id: 11,
-      text: "Okay, I'll try that.",
-    },
-    {
-      id: 12,
-      text: 'Did that work?',
-    },
-    {
-      id: 13,
-      text: "No, it didn't. I'm still getting the same error.",
-    },
-    {
-      id: 14,
-      text: "Okay, I'm not sure what's going on. Let me do some research and I'll get back to you.",
-    },
-    {
-      id: 15,
-      text: 'Okay, thank you!',
-    },
-    {
-      id: 16,
-      text: "Okay, I'm back. Are you using npm or yarn?",
-    },
-    {
-      id: 17,
-      text: "I'm using npm.",
-    },
-    {
-      id: 18,
-      text: 'Okay, try running this command: npm install -g @vue/devtools',
-    },
-  ]
+  const { supportChat } = useSupportContext()
 
   useEffect(() => {
     messagesRef.current?.scrollIntoView({ behavior: 'smooth' })
@@ -112,7 +40,7 @@ const SupportPage = () => {
           </div>
         </div> */}
 
-        {support.map((message, index) => (
+        {supportChat.map((message: any, index: number) => (
           <div
             key={index}
             className={`flex items-end ${
