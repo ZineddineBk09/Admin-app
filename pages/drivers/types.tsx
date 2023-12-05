aeca793bf02246f3442fba41f127d121971529bb
ZineddineBk09 <eino1209@gmail.com>
2023-12-05 08:09:54 +0100

    drivers types page

---
+++ pages/drivers/types.tsx
@@ -0,0 +1,9 @@
+import DriversTypes from '@/components/drivers/drivers-types'
+import { NextPage } from 'next'
+import React from 'react'
+
+const Drivers: NextPage = () => {
+  return <DriversTypes />
+}
+
+export default Drivers
