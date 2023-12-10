c0f26cc0cc1e3c0f5583417462a93e9a0c6b0da1
ZineddineBk09 <eino1209@gmail.com>
2023-12-10 11:19:24 +0100

    fix

--- components/icons/orders/index.tsx
+++ components/icons/orders/index.tsx
@@ -129,3 +129,24 @@ export const BagCrossedIcon = ({ color }: { color?: string }) => {
     </Svg>
   )
 }
+
+export const CancelIcon = ({ color }: { color?: string }) => {
+  return (
+    <Svg
+      width='20'
+      height='20'
+      viewBox='0 0 20 20'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      className='w-5'
+    >
+      <path
+        d='M6 10H14M10 19C5.02944 19 1 14.9706 1 10C1 5.02944 5.02944 1 10 1C14.9706 1 19 5.02944 19 10C19 14.9706 14.9706 19 10 19Z'
+        stroke='#F36A6A'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+    </Svg>
+  )
+}
