4997d8aef9a82e2d77b6ad71272ab2b4572eee85
ZineddineBk09 <eino1209@gmail.com>
2023-12-12 08:22:52 +0100

    fix

--- components/table/orders-table.tsx
+++ components/table/orders-table.tsx
@@ -1,6 +1,4 @@
-import { Table } from '@nextui-org/react'
 import React, { useEffect, useState } from 'react'
-import { Box } from '../styles/box'
 import { RenderCell } from './render-orders-cell'
 import { ordersTableCols } from './data'
 import { useOrdersContext } from '@/context/order'
@@ -23,7 +21,7 @@ export const OrdersTable = () => {
             <table className='min-w-full text-left text-sm font-light'>
               <thead className='border-b font-medium border-gray-300'>
                 <tr>
-                  {ordersTableCols.map((col) => (
+                  {ordersTableCols?.map((col) => (
                     <th key={col.uid} scope='col' className='px-6 py-4'>
                       {col.name === 'Actions' ? '' : col.name}
                     </th>
@@ -31,7 +29,7 @@ export const OrdersTable = () => {
                 </tr>
               </thead>
               <tbody>
-                {orders.map((order: any, index: number) => (
+                {orders?.map((order: any, index: number) => (
                   <tr
                     key={order.id}
                     // make table striped by adding bg-gray-50 to odd rows
@@ -39,7 +37,7 @@ export const OrdersTable = () => {
                       ${index % 2 === 0 ? ' bg-gray-200' : ''}
                     `}
                   >
-                    {ordersTableCols.map((col) => (
+                    {ordersTableCols?.map((col) => (
                       <td
                         key={col.uid}
                         className={`whitespace-nowrap px-6 py-4 font-medium border-gray-300 ${
@@ -59,83 +57,3 @@ export const OrdersTable = () => {
     </div>
   )
 }
-
-// <Box
-//   css={{
-//     '& .nextui-table-container': {
-//       boxShadow: 'none',
-//       marginBottom: '10px',
-//       borderRadius: '0',
-//       border: '1px solid #CECECE',
-//     },
-
-//     '& .nextui-table-header-row': {
-//       backgroundColor: '#000 !important',
-//       borderRadius: 0,
-//       height: '50px',
-//     },
-//   }}
-// >
-//   <Table
-//     aria-label='Orders table'
-//     hoverable
-//     striped
-//     css={{
-//       height: 'auto',
-//       minWidth: '100%',
-//       boxShadow: 'none',
-//       width: '100%',
-//       px: 5,
-//       backgroundColor: 'white',
-//     }}
-//     onSortChange={(sort: any) => {
-//       // because the table does not support descending sorting
-//       // we need to do it manually
-//       const { direction } = sorting
-//       const { column } = sort
-//       if (direction === 'ascending') {
-//         setSorting({
-//           column,
-//           direction: 'descending',
-//         })
-//         return
-//       }
-//       setSorting({ column, direction: 'ascending' })
-//     }}
-//   >
-//     <Table.Header columns={ordersTableCols}>
-//       {(column) => (
-//         <Table.Column
-//           key={column.uid}
-//           hideHeader={column.uid === 'actions'}
-//           align={column.uid === 'actions' ? 'center' : 'start'}
-//           allowsSorting={
-//             column.uid === 'completedTasks' ||
-//             column.uid === 'inProgressTasks'
-//           }
-//         >
-//           {column.name}
-//         </Table.Column>
-//       )}
-//     </Table.Header>
-//     <Table.Body items={orders}>
-//       {(item) => (
-//         <Table.Row>
-//           {(columnKey) => (
-//             <Table.Cell>
-//               {RenderCell({ order: item, columnKey: columnKey })}
-//             </Table.Cell>
-//           )}
-//         </Table.Row>
-//       )}
-//     </Table.Body>
-//     <Table.Pagination
-//       shadow
-//       noMargin
-//       align='center'
-//       color='warning'
-//       rowsPerPage={8}
-//       onPageChange={(page) => console.log({ page })}
-//     />
-//   </Table>
-// </Box>
