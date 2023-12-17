9f6c72d93de581a7af7a6aaa5f3119a0a7700d43
ZineddineBk09 <eino1209@gmail.com>
2023-12-17 09:24:56 +0100

    access profiles

---
+++ pages/settings/access-profiles.tsx
@@ -0,0 +1,9 @@
+import Profiles from '@/components/settings/access-profiles'
+import { NextPage } from 'next'
+import React from 'react'
+
+const AccessProfiles: NextPage = () => {
+  return <Profiles />
+}
+
+export default AccessProfiles
