601db8e98b973cc253405cff9ac17dd9477761ca
ZineddineBk09 <eino1209@gmail.com>
2023-12-18 08:10:21 +0100

    fix

--- components/clients/accounts/index.tsx
+++ components/clients/accounts/index.tsx
@@ -1,12 +1,12 @@
-import { Account, Branch } from '@/interfaces'
-import { Divider } from '@nextui-org/react'
+import { Account } from '@/interfaces'
+import { Divider, Tooltip } from '@nextui-org/react'
 import React from 'react'
 import { AddAccount } from './add-account'
 import { DeleteAccount } from './delete-account'
-import CopyToClipboardButton from '@/components/shared/copy-to-clipboard'
 import { ChevronRightIcon } from '@heroicons/react/24/outline'
 import { useClientsAccountsContext } from '@/context/clients/accounts'
 import Link from 'next/link'
+import { BinIcon } from '@/components/icons/areas'
 
 const Accounts = () => {
   const { accounts } = useClientsAccountsContext()
@@ -27,7 +27,8 @@ const Accounts = () => {
 
 const AccountCard = ({ account }: { account: Account }) => {
   const [showInfos, setShowInfos] = React.useState(false)
-  const { id, name, city, discount, website, phone, branches } = account
+  const { id, name, city, discount, website, phone, branches, teams, admins } =
+    account
   const fields = [
     {
       name: 'Discount',
@@ -65,36 +66,118 @@ const AccountCard = ({ account }: { account: Account }) => {
       </div>
       {showInfos && (
         <>
-          <Divider></Divider>
+          <Divider />
           <div className='w-full flex items-center gap-x-6'>
             <label className='text-gray-600 text-sm'>Name</label>
             <p className='text-sm'>{name}</p>
           </div>
-          <Divider></Divider>
+          <Divider />
           <div className='w-full flex items-center gap-x-6'>
             <label className='text-gray-600 text-sm'>City</label>
             <p className='text-sm'>{city}</p>
           </div>
-          <Divider></Divider>
-          {fields?.map(({ name, id, defaultValue }: any, index: number) => (
-            <>
-              <div key={index} className='w-full flex items-center gap-x-6'>
-                <label className='text-gray-600 text-sm'>{name}</label>
-                <input
-                  name={id}
-                  id={id}
-                  type='text'
-                  value={defaultValue}
-                  onChange={(e) => {
-                    console.log(e.target.value)
-                  }}
-                  className='w-60 bg-gray-200 rounded-md p-2 text-sm'
-                />
-                <CopyToClipboardButton text={defaultValue} />
+          <Divider />
+          <div className='w-full flex items-center justify-between'>
+            {fields?.map(({ name, id, defaultValue }: any, index: number) => (
+              <>
+                <div key={index} className='h-11 flex items-center gap-x-6'>
+                  <label className='text-gray-600 text-sm'>{name}</label>
+                  <input
+                    name={id}
+                    id={id}
+                    type='text'
+                    value={defaultValue}
+                    onChange={(e) => {
+                      console.log(e.target.value)
+                    }}
+                    className='w-60 h-full bg-gray-200 rounded-md p-2 text-sm'
+                  />
+                </div>
+              </>
+            ))}
+          </div>
+          <Divider />
+
+          {/* Teams */}
+          <div className='w-full flex items-start gap-x-6'>
+            <label className='mt-2 text-gray-600 text-sm'>Teams</label>
+            {teams.length > 0 ? (
+              <div className='w-full flex items-start gap-y-2'>
+                {teams?.map((team: any, index: number) => (
+                  <div key={index}>
+                    <div className='h-10 w-fit flex items-center gap-x-6 transition-all duration-300 hover:bg-gray-100 px-2 rounded-md'>
+                      {/* <label className='text-gray-600 text-sm'>
+                        Area #{index + 1}
+                      </label> */}
+                      <p className='text-sm capitalize'>
+                        {team.name}{' '}
+                        {/* <span className='ml-6 text-sm text-gray-400'>
+                          #{index}
+                        </span> */}
+                      </p>
+                      <Tooltip
+                        content={'Delete "' + team.name + '"'}
+                        color='error'
+                      >
+                        <button>
+                          <BinIcon width={4} />
+                        </button>
+                      </Tooltip>
+                      {index < teams.length - 1 && (
+                        <span className='-ml-4'>,</span>
+                      )}
+                    </div>
+                  </div>
+                ))}
               </div>
-              <Divider></Divider>
-            </>
-          ))}
+            ) : (
+              <p className='text-sm'>No teams found</p>
+            )}
+
+            <Tooltip content='Add Team'>
+              <button className='h-10 w-16 flex items-center justify-center text-center text-4xl font-medium rounded-full'>
+                +
+              </button>
+            </Tooltip>
+          </div>
+
+          <Divider />
+          {/* Admins */}
+          <div className='w-full flex items-start gap-x-6'>
+            <label className='mt-2 text-gray-600 text-sm'>Admins</label>
+            {admins.length > 0 ? (
+              <div className='w-full flex items-start gap-y-2'>
+                {admins?.map((admin: any, index: number) => (
+                  <div key={index}>
+                    <div className='h-10 w-fit flex items-center gap-x-6 transition-all duration-300 hover:bg-gray-100 px-2 rounded-md'>
+                      <p className='text-sm capitalize'>{admin.name} </p>
+                      <Tooltip
+                        content={'Delete "' + admin.name + '"'}
+                        color='error'
+                      >
+                        <button>
+                          <BinIcon width={4} />
+                        </button>
+                      </Tooltip>
+                      {index < admins.length - 1 && (
+                        <span className='-ml-4'>,</span>
+                      )}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p className='text-sm'>No admins found</p>
+            )}
+
+            <Tooltip content='Add Team'>
+              <button className='h-10 w-16 flex items-center justify-center text-center text-4xl font-medium rounded-full'>
+                +
+              </button>
+            </Tooltip>
+          </div>
+
+          <Divider />
 
           <div className='w-full flex items-start gap-x-6'>
             <label className='mt-2 text-gray-600 text-sm'>Branches</label>
@@ -127,7 +210,8 @@ const AccountCard = ({ account }: { account: Account }) => {
               <p className='text-sm'>No branches found</p>
             )}
           </div>
-          <Divider></Divider>
+
+          <Divider />
 
           <div className='w-full flex items-center gap-x-6'>
             <label className='text-gray-600 text-sm'>Contract</label>
