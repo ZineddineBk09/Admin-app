35e39fdda13a049abbea788b24b3637a09061302
ZineddineBk09 <eino1209@gmail.com>
2023-12-18 08:09:43 +0100

    fix

--- components/areas/governorates/add-governorate.tsx
+++ components/areas/governorates/add-governorate.tsx
@@ -60,16 +60,16 @@ export const AddGovernorate = () => {
           <Loading size='xl' className='my-3' color='warning' />
         ) : (
           <form onSubmit={formik.handleSubmit}>
-            <Modal.Header css={{ justifyContent: 'start' }}>
+            <Modal.Header css={{ justifyContent: 'center' }}>
               <Text
                 id='modal-title'
-                className='text-xl font-semibold uppercase'
+                className='text-xl font-semibold capitalize'
                 h4
               >
                 Add Governorate
               </Text>
             </Modal.Header>
-            <Divider css={{ my: '$5' }} />
+            {/* <Divider css={{ my: '$5' }} /> */}
             <Modal.Body css={{ py: '$10' }}>
               <Flex
                 direction={'column'}
@@ -179,7 +179,7 @@ export const AddGovernorate = () => {
                 </Flex>
               </Flex>
             </Modal.Body>
-            <Divider css={{ my: '$5' }} />
+            {/* <Divider css={{ my: '$5' }} /> */}
             <Modal.Footer>
               <Button auto type='submit' className='bg-primary text-black'>
                 Add Governorate
