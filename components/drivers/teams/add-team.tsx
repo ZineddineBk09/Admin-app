2459ce22cd5165658a59953c396ba1abe08e7e79
ZineddineBk09 <eino1209@gmail.com>
2023-12-18 08:11:57 +0100

    fixes

--- components/drivers/teams/add-team.tsx
+++ components/drivers/teams/add-team.tsx
@@ -1,6 +1,5 @@
 import {
   Button,
-  Divider,
   Input,
   Modal,
   Text,
@@ -60,16 +59,16 @@ export const AddDriverTeam = () => {
           <Loading size='xl' className='my-3' color='warning' />
         ) : (
           <form onSubmit={formik.handleSubmit}>
-            <Modal.Header css={{ justifyContent: 'start' }}>
+            <Modal.Header css={{ justifyContent: 'center' }}>
               <Text
                 id='modal-title'
-                className='text-xl font-semibold uppercase'
+                className='text-xl font-semibold capitalize'
                 h4
               >
                 Add Team
               </Text>
             </Modal.Header>
-            <Divider css={{ my: '$5' }} />
+            {/* <Divider css={{ my: '$5' }} /> */}
             <Modal.Body css={{ py: '$10' }}>
               <Flex
                 direction={'column'}
@@ -179,7 +178,7 @@ export const AddDriverTeam = () => {
                 </Flex>
               </Flex>
             </Modal.Body>
-            <Divider css={{ my: '$5' }} />
+            {/* <Divider css={{ my: '$5' }} /> */}
             <Modal.Footer>
               <Button auto type='submit' className='bg-primary text-black'>
                 Add Team
