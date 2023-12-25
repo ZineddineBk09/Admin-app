1ea7abc64e7b786d1c39ac1a9b1d738cf7ef2009
ZineddineBk09 <eino1209@gmail.com>
2023-12-25 09:37:43 +0100

    fix

--- pages/settings/access-profiles.tsx
+++ pages/settings/access-profiles.tsx
@@ -2,8 +2,8 @@ import Profiles from '@/components/settings/access-profiles'
 import { NextPage } from 'next'
 import React from 'react'
 
-const AccessProfiles: NextPage = () => {
+const AccessProfilesPage: NextPage = () => {
   return <Profiles />
 }
 
-export default AccessProfiles
+export default AccessProfilesPage
