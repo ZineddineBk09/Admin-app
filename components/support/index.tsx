db73ccb13879c8d647aeae2fa80180e6901632c5
ZineddineBk09 <eino1209@gmail.com>
2023-12-26 08:59:14 +0100

    fixes

--- components/support/index.tsx
+++ components/support/index.tsx
@@ -1,38 +1,27 @@
 import { useSupportContext } from '@/context/support/support-context'
-import { SupportTeamMember } from '@/interfaces'
+import { Chat, ChatMessage } from '@/interfaces'
 import React, { useEffect } from 'react'
 import { AttachmentIcon, EmojiIcon, SendIcon } from '../icons/support'
 import { Tooltip } from '@nextui-org/react'
 import { useFormik } from 'formik'
 import { useSession } from 'next-auth/react'
+import EmojiPicker from 'emoji-picker-react'
+import Emojis from './emoji-picker'
+import { addMessage } from '@/lib/api/support'
+import { string } from 'yup'
 
 const SupportPage = () => {
   return (
     <div className='w-full flex items-start justify-between overflow-y-hidden h-[92vh]'>
       <SupportMembers />
-      <Chat />
+      <ChatContent />
     </div>
   )
 }
 
-const Chat = () => {
+const ChatContent = () => {
   const messagesRef: any = React.useRef(null)
-  const { selectedMember } = useSupportContext()
-
-  // get chat messages and group them by date
-  const chatMessages = selectedMember?.chats?.reduce(
-    (acc: any, message: any) => {
-      const date = new Date(message.date)
-      const dateKey = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
-      if (acc[dateKey]) {
-        acc[dateKey].push(message)
-      } else {
-        acc[dateKey] = [message]
-      }
-      return acc
-    },
-    {}
-  )
+  const { selectedChat, chatMessages } = useSupportContext()
 
   useEffect(() => {
     messagesRef.current?.scrollIntoView({ behavior: 'smooth' })
@@ -45,57 +34,68 @@ const Chat = () => {
         ref={messagesRef}
         className='flex flex-col space-y-4 p-3 overflow-y-auto pb-36'
       >
-        {chatMessages &&
-          Object.keys(chatMessages).map((dateKey: any, index: number) => (
+        {/* {chatMessages &&
+          Object.keychatMessages.map((dateKey: any, index: number) => (
             <>
               <span className='w-28 mx-auto bg-gray-300 py-1 font-medium shadow-lg rounded-full text-center text-gray-600 text-sm md:text-base md:w-32 lg:w-36'>
                 {dateKey}
+              </span> */}
+        {chatMessages?.map((message: ChatMessage, index: number) => (
+          <div
+            key={index}
+            className={`flex items-end ${
+              message.type === 'customer' ? 'justify-end' : 'justify-start'
+            }`}
+          >
+            <div
+              className={`flex flex-col text-sm max-w-2xl mx-2  ${
+                message.type === 'customer'
+                  ? 'order-1 items-end'
+                  : 'order-2 items-start'
+              } `}
+            >
+              <span
+                className={`w-full h-5 px-4 text-xs font-bold flex items-center rounded-t-md shadow-md text-white ${
+                  message.type === 'customer'
+                    ? 'bg-gray-400 justify-end'
+                    : 'bg-blue-600'
+                }`}
+              >
+                {message.type === 'customer' ? (
+                  <>
+                    {selectedChat.customerName} <div className='mx-3' />
+                    {new Date(
+                      message.timestamp.seconds * 1000
+                    ).toLocaleTimeString('en-US', {
+                      hour: '2-digit',
+                      minute: '2-digit',
+                    })}
+                  </>
+                ) : (
+                  <>
+                    {new Date(
+                      message.timestamp.seconds * 1000
+                    ).toLocaleTimeString('en-US', {
+                      hour: '2-digit',
+                      minute: '2-digit',
+                    })}{' '}
+                    <div className='mx-3' />
+                    {message.senderId}
+                  </>
+                )}
               </span>
-              {chatMessages[dateKey]?.map((message: any, index: number) => (
-                <div
-                  key={index}
-                  className={`flex items-end ${
-                    index % 2 === 0 ? 'justify-end' : 'justify-start'
-                  }`}
-                >
-                  <div
-                    className={`flex flex-col text-sm max-w-2xl mx-2  ${
-                      index % 2 === 0
-                        ? 'order-1 items-end'
-                        : 'order-2 items-start'
-                    } `}
-                  >
-                    <span
-                      className={`w-full h-5 px-4 text-xs font-bold flex items-center rounded-t-md shadow-md text-white ${
-                        index % 2 === 0
-                          ? 'bg-gray-400 justify-end'
-                          : 'bg-blue-600'
-                      }`}
-                    >
-                      {index % 2 === 0 ? (
-                        <>
-                          Customer <div className='mx-3' />
-                          12:00 PM
-                        </>
-                      ) : (
-                        <>
-                          12:00 PM <div className='mx-3' />
-                          {selectedMember.name}
-                        </>
-                      )}
-                    </span>
-                    <span
-                      className={`w-full px-4 py-2 rounded-b-md inline-block text-black shadow-md ${
-                        index % 2 === 0 ? 'bg-gray-200' : 'bg-[#59AFFF]'
-                      }`}
-                    >
-                      {message.text}
-                    </span>
-                  </div>
-                </div>
-              ))}
-            </>
-          ))}
+              <span
+                className={`w-full px-4 py-2 rounded-b-md inline-block text-black shadow-md ${
+                  message.type === 'customer' ? 'bg-gray-200' : 'bg-[#59AFFF]'
+                }`}
+              >
+                {message.content}
+              </span>
+            </div>
+          </div>
+        ))}
+        {/* </>
+          ))} */}
       </div>
 
       {/* Message Section */}
@@ -108,14 +108,23 @@ const Chat = () => {
 }
 
 const ChatForm = () => {
-  const { data: session} = useSession()
-  
+  const { data: session } = useSession()
+  const { selectedChat } = useSupportContext()
+
   const formik = useFormik({
     initialValues: {
       message: '',
     },
-    onSubmit: (values) => {
+    onSubmit: async (values) => {
       console.log(values)
+      await addMessage({
+        content: values.message,
+        chatId: selectedChat.id,
+        senderId: session?.user?.username,
+      }).then((id: string) => {
+        console.log(id)
+      })
+      formik.resetForm()
     },
   })
 
@@ -146,11 +155,9 @@ const ChatForm = () => {
           </button>
           <button
             type='button'
-            className='inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none'
+            className='relative inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none'
           >
-            <Tooltip content='Add an emoji' color='warning'>
-              <EmojiIcon />
-            </Tooltip>
+            {/* <Emojis /> */}
           </button>
         </div>
         <button
@@ -167,27 +174,26 @@ const ChatForm = () => {
 }
 
 const SupportMembers = () => {
-  const { supportTeam, selectedMember, handleSelectMember } =
-    useSupportContext()
+  const { chats, selectedChat, handleSelectChat } = useSupportContext()
 
   return (
     <ul className='w-[300px] h-full list-none bg-gray-200 min-h-full !overflow-y-scroll'>
-      {supportTeam.map(
-        ({ id, name, unread }: SupportTeamMember, index: number) => (
+      {chats?.map(
+        ({ id, customerName, unread, lastUpdate }: Chat, index: number) => (
           <li
             className='px-4 border-b border-gray-300 relative cursor-pointer'
             key={index}
-            onClick={() => handleSelectMember(id)}
+            onClick={() => handleSelectChat(id)}
           >
             <div className='flex items-center justify-between relative py-3'>
               <span
                 className={`${
-                  selectedMember.id === id
+                  selectedChat.id === id
                     ? 'text-black font-bold'
                     : 'text-gray-400 font-medium'
                 }`}
               >
-                {name}
+                {customerName}
               </span>
               {unread > 0 ? (
                 <span className='h-6 w-6 flex items-center justify-center text-center text-white font-bold text-xs bg-red-500 rounded-full'>
@@ -197,7 +203,7 @@ const SupportMembers = () => {
                 <span className='h-6 w-6' />
               )}
             </div>
-            {selectedMember.id === id && (
+            {selectedChat.id === id && (
               <div className='absolute w-3 h-full top-0 right-0 bg-primary transition-all duration-300' />
             )}
           </li>
