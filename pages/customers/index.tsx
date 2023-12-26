d8f2971d54c5337ac5cf678b5625d5cbaaaebc9f
ZineddineBk09 <eino1209@gmail.com>
2023-12-26 09:00:10 +0100

    fix

--- pages/customers/index.tsx
+++ pages/customers/index.tsx
@@ -1,8 +1,8 @@
 import { NextPage } from 'next'
 import React from 'react'
 
-const Customers: NextPage = () => {
+const CustomersPage: NextPage = () => {
   return <div></div>
 }
 
-export default Customers
+export default CustomersPage
