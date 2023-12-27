853779667ac71bdba9d71269e9ad2781fd82da6e
ZineddineBk09 <eino1209@gmail.com>
2023-12-27 14:38:17 +0100

    emoji picker

--- components/support/emoji-picker.tsx
+++ components/support/emoji-picker.tsx
@@ -1,24 +1,59 @@
+import { useEffect, useRef, useState } from 'react'
 import { Tooltip } from '@nextui-org/react'
 import React from 'react'
 import { EmojiIcon } from '../icons/support'
-import EmojiPicker from 'emoji-picker-react'
+import EmojiPicker, { EmojiStyle, EmojiClickData } from 'emoji-picker-react'
 
-const Emojis = () => {
-  const [showEmojiPicker, setShowEmojiPicker] = React.useState(false)
-  const [chosenEmoji, setChosenEmoji] = React.useState(null)
+const Emojis = ({
+  onSelectEmojis,
+}: {
+  onSelectEmojis: (emoji: any) => void
+}) => {
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false)
+  const [chosenEmoji, setChosenEmoji] = useState<any>(null)
+  const emojiPickerRef = useRef<HTMLDivElement>(null)
+
+  function handleClickOutside(event: MouseEvent) {
+    if (
+      emojiPickerRef.current &&
+      !emojiPickerRef.current.contains(event.target as Node)
+    ) {
+      setShowEmojiPicker(false)
+    }
+  }
+
+  function onClick(emojiData: EmojiClickData, event: MouseEvent) {
+    setChosenEmoji(emojiData)
+    setShowEmojiPicker(false)
+    onSelectEmojis(emojiData.emoji)
+  }
+
+  useEffect(() => {
+    // Attach event listener when emoji picker is shown
+    if (showEmojiPicker) {
+      document.addEventListener('mousedown', handleClickOutside)
+    }
+
+    // Cleanup when component unmounts or emoji picker is hidden
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [showEmojiPicker])
 
   return (
     <Tooltip content={showEmojiPicker ? '' : 'Add an emoji'} color='warning'>
-      <button onClick={() => setShowEmojiPicker(!showEmojiPicker)}>
+      <button
+        type='button'
+        onClick={() => setShowEmojiPicker(!showEmojiPicker)}
+      >
         <EmojiIcon />
       </button>
       {showEmojiPicker && (
-        <div className='absolute bottom-4 -left-24'>
+        <div ref={emojiPickerRef} className='absolute bottom-32 -left-24'>
           <EmojiPicker
-            onEmojiClick={(event, emojiObject) =>
-              console.log('emoji: ', emojiObject.names)
-            }
-            style={{ position: 'absolute', bottom: '100px' }}
+            onEmojiClick={onClick}
+            autoFocusSearch={false}
+            emojiStyle={EmojiStyle.NATIVE}
           />
         </div>
       )}
