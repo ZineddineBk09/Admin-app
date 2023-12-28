91eed610308dade3c9fde977f979a84259c60453
ZineddineBk09 <eino1209@gmail.com>
2023-12-28 08:36:42 +0100

    fixes

--- context/support/support-context.tsx
+++ context/support/support-context.tsx
@@ -1,6 +1,7 @@
 import { firestore } from '@/firebase/support'
 import { Chat, ChatMessage, SupportTeamMember } from '@/interfaces'
-import { collection, onSnapshot } from 'firebase/firestore'
+import { fetchChatMessages } from '@/lib/api/support'
+import { collection, collectionGroup, onSnapshot } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 
 export const SupportContext = React.createContext({})
@@ -8,9 +9,7 @@ export const SupportContext = React.createContext({})
 export const useSupportContext: {
   (): {
     chats: Chat[]
-    supportTeamMembers: SupportTeamMember[]
     chatMessages: ChatMessage[]
-    supportTeam: SupportTeamMember[]
     selectedChat: Chat
     loading: boolean
     handleSelectChat: (id: string) => void
@@ -22,23 +21,28 @@ export const SupportContextProvider = ({
 }: {
   children: React.ReactNode
 }) => {
-  const [supportTeam, setSupportTeam] = useState<SupportTeamMember[]>(
-    [] as SupportTeamMember[]
-  )
   const [selectedChat, setSelectedChat] = useState<Chat>({} as Chat)
   const [chats, setChats] = useState<Chat[]>([])
-  const [supportTeamMembers, setSupportTeamMembers] = useState<
-    SupportTeamMember[]
-  >([])
   const [chatMessages, setChatMessages] = useState<ChatMessage[]>([])
-
   const [loading, setLoading] = useState(false)
 
-  const handleSelectChat = (id: string) => {
+  const handleSelectChat = async (id: string) => {
     const chat = chats?.find((chat) => chat.id === id)
 
     if (chat) {
+      setLoading(true)
+      // track adding new messages in chat messages subcollection
+      const unsub = onSnapshot(
+        collectionGroup(firestore, 'messages'),
+        async (snapshot: any) => {
+          const messages: any[] = await fetchChatMessages(id)
+          setChatMessages(messages)
+          setLoading(false)
+        }
+      )
       setSelectedChat(chat)
+      setLoading(false)
+      return unsub
     }
   }
 
@@ -73,19 +77,10 @@ export const SupportContextProvider = ({
             return prevChats
           })
 
-          // set selected chat
+          // set selected chat if it's not set
           if (!selectedChat.id) {
-            setSelectedChat({
-              id: change.doc.id,
-              ...change.doc.data(),
-            } as Chat)
-          } else {
-            if (selectedChat.id === change.doc.id) {
-              setSelectedChat({
-                id: change.doc.id,
-                ...change.doc.data(),
-              } as Chat)
-            }
+            console.log('selecting chat')
+            handleSelectChat(change.doc.id)
           }
         })
       })
@@ -97,61 +92,11 @@ export const SupportContextProvider = ({
     getChats()
   }, [])
 
-  // MESSAGES
-  useEffect(() => {
-    const getMessages = () => {
-      const unsub = onSnapshot(
-        collection(firestore, 'messages'),
-        (snapshot) => {
-          snapshot.docChanges().forEach((change) => {
-            
-            setChatMessages((prevMessages: any) => {
-              if (change.type === 'added') {
-                return [
-                  ...prevMessages.filter(
-                    (message: ChatMessage) => message.id !== change.doc.id
-                  ),
-                  {
-                    id: change.doc.id,
-                    ...change.doc.data(),
-                  } as ChatMessage,
-                ].sort((a: ChatMessage, b: ChatMessage) =>
-                  a.timestamp.seconds > b.timestamp.seconds ? 1 : -1
-                )
-              } else if (change.type === 'modified') {
-                return prevMessages.map((message: ChatMessage) =>
-                  message.id === change.doc.id
-                    ? {
-                        id: change.doc.id,
-                        ...change.doc.data(),
-                      }
-                    : message
-                )
-              } else if (change.type === 'removed') {
-                return prevMessages.filter(
-                  (message: ChatMessage) => message.id !== change.doc.id
-                ) as ChatMessage[]
-              }
-              return prevMessages
-            })
-          })
-        }
-      )
-      
-      return () => {
-        unsub()
-      }
-    }
-    getMessages()
-  }, [])
-
   return (
     <SupportContext.Provider
       value={{
         chats,
-        supportTeamMembers,
         chatMessages,
-        supportTeam,
         selectedChat,
         loading,
         handleSelectChat,
