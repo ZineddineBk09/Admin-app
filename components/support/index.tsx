7762ad9af730d65664a6978c6f1c4937965f2e7e
ZineddineBk09 <eino1209@gmail.com>
2023-12-29 08:18:57 +0100

    fix

--- components/support/index.tsx
+++ components/support/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
-import { ChatContent } from './chat'
-import { SupportMembers } from './support-members'
+import { ChatContent } from './chat-content'
+import { Chats } from './chats'
 
 const SupportPage = () => {
   return (
     <div className='w-full flex items-start justify-between overflow-y-hidden h-[92vh]'>
-      <SupportMembers />
+      <Chats />
       <ChatContent />
     </div>
   )
 }
-
-
-
 
 export default SupportPage
