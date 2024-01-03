12c66d214f5f37ff799f7f54f1ced4a69bb2f7a1
ZineddineBk09 <eino1209@gmail.com>
2024-01-03 09:06:00 +0100

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,7 @@ import LoginForm from '../components/login/login-form'
 import React from 'react'
 
 const Login: NextPage = () => {
-  return (
-    <>
-      <LoginForm />
-    </>
-  )
+  return <LoginForm />
 }
 
 export default Login
