da4e33825b9d4c73da0b7485e66b3c4cbac4adbc
ZineddineBk09 <eino1209@gmail.com>
2024-01-08 10:11:01 +0100

    fix

--- firebase/support.ts
+++ firebase/support.ts
@@ -2,7 +2,6 @@ import { initializeApp, getApp, getApps } from 'firebase/app'
 import { getStorage } from 'firebase/storage'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
-import { getAnalytics } from 'firebase/analytics'
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,9 +13,8 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
-// Initialize Firebase this way to avoid multiple instances of firebase app being created
+// Initialize Firebase this way to avoid multiple instances of firebase app being created 
 export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 export const firestore = getFirestore(app)
 export const storage = getStorage(app)
 export const auth = getAuth(app)
-// export const analytics = getAnalytics(app)
