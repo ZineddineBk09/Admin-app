f4675a037d343c078533bcb1f22b68f40f3fa283
ZineddineBk09 <eino1209@gmail.com>
2024-01-12 08:30:35 +0100

    fix

--- components/areas/cities/index.tsx
+++ components/areas/cities/index.tsx
@@ -17,12 +17,14 @@ const Cities = () => {
     <div className='w-full mx-auto flex flex-col items-center gap-y-6'>
       <SearchCity />
 
-      {/*  */}
+      {/* Cities list */}
       <div className='w-full flex flex-col items-center gap-y-6'>
         {cities?.map((city: City, index: number) => (
           <CityCard key={index} city={city} />
         ))}
       </div>
+
+      {/* Add city btn */}
       <AddCity />
     </div>
   )
