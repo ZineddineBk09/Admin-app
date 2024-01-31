71a46f94db1b0c109381f421eb02630b6f7fd862
ZineddineBk09 <eino1209@gmail.com>
2024-01-31 17:39:57 +0100

    fix

--- pages/areas/cities.tsx
+++ pages/areas/cities.tsx
@@ -1,7 +1,13 @@
-import Cities from '@/components/areas/cities'
-import { AreasCitiesContextProvider } from '@/context/areas/cities'
+// import Cities from '../components/areas/cities'
+import dynamic from 'next/dynamic'
+const Cities = dynamic(() => import('../../components/areas/cities'), {
+  ssr: false,
+  loading: () => <Loading />,
+})
+import { AreasCitiesContextProvider } from '../../context/areas/cities'
 import { NextPage } from 'next'
 import React from 'react'
+import Loading from '../../components/shared/loading'
 
 const AreasCitiesPage: NextPage = () => {
   return (
