ce142340c19c2b59b6d1ed65cf4ba2265cae68c7
ZineddineBk09 <eino1209@gmail.com>
2024-02-01 10:11:57 +0100

    fix

---
+++ context/areas/countries.tsx
@@ -0,0 +1,70 @@
+import { City } from '../../interfaces'
+import React, { useEffect, useState } from 'react'
+
+export const AreasCitiesContext = React.createContext({})
+
+export const useAreasCitiesContext: any = () =>
+  React.useContext(AreasCitiesContext)
+
+export const AreasCitiesContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode
+}) => {
+  const [cities, setCities] = useState<City[]>([] as City[])
+  const countries = ['Saudi Arabia', 'United Arab Emirates', 'Egypt']
+  const [loading, setLoading] = useState(true)
+
+  const refreshCities = async () => {
+    // setLoading(true)
+    // setCities([] as City[])
+    // const records = await fetchCities()
+    // setCities(records)
+    // setLoading(false)
+    setCities([
+      {
+        id: '215351',
+        name: 'البجيري - Al Bujairi',
+        governorateId: '215351',
+        governorateName: 'Riyadh',
+        price: 25,
+        additional: 4,
+        orderFee: 10,
+      },
+      {
+        id: '215352',
+        name: 'الروضة - Ar Rawdah',
+        governorateId: '215352',
+        governorateName: 'Jeddah',
+        price: 25,
+        additional: 4,
+        orderFee: 10,
+      },
+      {
+        id: '215353',
+        name: 'الروضة - Ar Rawdah',
+        governorateId: '215353',
+        governorateName: 'Riyadh',
+        price: 25,
+        additional: 4,
+        orderFee: 10,
+      },
+    ])
+  }
+
+  useEffect(() => {
+    refreshCities()
+  }, [])
+
+  return (
+    <AreasCitiesContext.Provider
+      value={{
+        cities,
+        countries,
+        loading,
+      }}
+    >
+      {children}
+    </AreasCitiesContext.Provider>
+  )
+}
