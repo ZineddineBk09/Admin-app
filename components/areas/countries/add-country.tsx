0e2498cbccfc92e1d88839a059e44b7e4e2a4835
ZineddineBk09 <eino1209@gmail.com>
2024-02-03 08:57:45 +0100

    integrate with api

--- components/areas/countries/add-country.tsx
+++ components/areas/countries/add-country.tsx
@@ -4,27 +4,50 @@ import { Flex } from '../../styles/flex'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import { AddIcon } from '../../../components/icons/areas'
+import { createRecord } from '../../../lib/api'
+import toast from 'react-hot-toast'
+import { useAreasCountriesContext } from '../../../context/areas/countries'
+import { Currency } from '../../../interfaces'
 
 export const AddCountry = () => {
   const [visible, setVisible] = React.useState(false)
   const handler = () => setVisible(true)
   const [loading, setLoading] = React.useState<boolean>(false)
+  const { currencies, refreshCountries } = useAreasCountriesContext()
 
   const formik = useFormik({
     initialValues: {
       name: '',
-      priceUnit: '',
-      orderFees: 0,
-      driverFees: 0,
+      price_unit: '',
+      order_fees: 0,
+      driver_fees: 0,
     },
     validationSchema: Yup.object({
       name: Yup.string().required('name is required'),
-      priceUnit: Yup.string().required('price unit is required'),
-      orderFees: Yup.number().required('order fee is required'),
-      driverFees: Yup.number().required('driver fee is required'),
+      price_unit: Yup.string().required('price unit is required'),
+      order_fees: Yup.number().required('order fee is required'),
+      driver_fees: Yup.number().required('driver fee is required'),
     }),
-    onSubmit: (values) => {
-      console.log(values)
+    onSubmit: async (values) => {
+      const price_unit = currencies?.find(
+        (currency: Currency) => currency.id === values.price_unit
+      )
+      if (!price_unit) {
+        toast.error('Price unit not found!')
+        return
+      }
+      await createRecord({ ...values, price_unit }, 'country')
+        .then((res) => {
+          if (res) {
+            setVisible(false)
+            toast.success('Country added successfully')
+            refreshCountries()
+          }
+        })
+        .catch((err) => {
+          console.log('Error adding country!: ', err)
+          toast.error('Error adding country!')
+        })
     },
   })
 
@@ -71,104 +94,99 @@ export const AddCountry = () => {
                   '@lg': { flexWrap: 'nowrap', gap: '$12' },
                 }}
               >
-                <Flex
-                  css={{
-                    gap: '$10',
-                    flexWrap: 'wrap',
-                    '@lg': { flexWrap: 'nowrap' },
-                  }}
-                >
-                  <Input
-                    label={
-                      formik.touched.name && formik.errors.name
-                        ? formik.errors.name
-                        : 'Name'
-                    }
-                    clearable
-                    fullWidth
-                    size='lg'
-                    placeholder='Name'
-                    name='name'
-                    id='name'
-                    value={formik.values.name}
+                <Input
+                  label={
+                    formik.touched.name && formik.errors.name
+                      ? formik.errors.name
+                      : 'Name'
+                  }
+                  clearable
+                  fullWidth
+                  size='lg'
+                  placeholder='Name'
+                  name='name'
+                  id='name'
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                  status={
+                    formik.touched.name && formik.errors.name
+                      ? 'error'
+                      : 'default'
+                  }
+                />
+                <div>
+                  <label
+                    className={`block mb-2 ${
+                      formik.touched.price_unit && formik.errors.price_unit
+                        ? 'text-red-500'
+                        : 'text-gray-900'
+                    }`}
+                  >
+                    {formik.touched.price_unit && formik.errors.price_unit
+                      ? formik.errors.price_unit
+                      : 'Price Unit'}
+                  </label>
+                  <select
+                    id='price_unit'
+                    name='price_unit'
                     onChange={formik.handleChange}
-                    status={
-                      formik.touched.name && formik.errors.name
-                        ? 'error'
-                        : 'default'
-                    }
-                  />
-                  <Input
-                    label={
-                      formik.touched.priceUnit && formik.errors.priceUnit
-                        ? formik.errors.priceUnit
-                        : 'Price Unit'
-                    }
-                    clearable
-                    fullWidth
-                    size='lg'
-                    placeholder='e.g: 1, 2, 3, 4'
-                    name='priceUnit'
-                    id='priceUnit'
-                    value={formik.values.priceUnit}
-                    onChange={formik.handleChange}
-                    status={
-                      formik.touched.priceUnit && formik.errors.priceUnit
-                        ? 'error'
-                        : 'default'
-                    }
-                  />
-                </Flex>
+                    value={formik.values.price_unit}
+                    className={`border  text-gray-900 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 ${
+                      formik.touched.price_unit && formik.errors.price_unit
+                        ? 'border-red-500 bg-red-200'
+                        : 'border-gray-300 bg-gray-100'
+                    }`}
+                  >
+                    <option value=''>Select Price Unit</option>
+                    {currencies?.map((currency: Currency) => (
+                      <option key={currency.id} value={currency.id}>
+                        {currency.symbol} - {currency.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
 
-                <Flex
-                  css={{
-                    gap: '$10',
-                    flexWrap: 'wrap',
-                    '@lg': { flexWrap: 'nowrap' },
-                  }}
-                >
-                  <Input
-                    label={
-                      formik.touched.orderFees && formik.errors.orderFees
-                        ? formik.errors.orderFees
-                        : 'Order Fees'
-                    }
-                    clearable
-                    fullWidth
-                    size='lg'
-                    placeholder='Stars'
-                    name='orderFees'
-                    id='orderFees'
-                    value={formik.values.orderFees}
-                    onChange={formik.handleChange}
-                    status={
-                      formik.touched.orderFees && formik.errors.orderFees
-                        ? 'error'
-                        : 'default'
-                    }
-                  />
+                <Input
+                  label={
+                    formik.touched.order_fees && formik.errors.order_fees
+                      ? formik.errors.order_fees
+                      : 'Order Fees'
+                  }
+                  clearable
+                  fullWidth
+                  size='lg'
+                  placeholder='Stars'
+                  name='order_fees'
+                  id='order_fees'
+                  value={formik.values.order_fees}
+                  onChange={formik.handleChange}
+                  status={
+                    formik.touched.order_fees && formik.errors.order_fees
+                      ? 'error'
+                      : 'default'
+                  }
+                />
 
-                  <Input
-                    label={
-                      formik.touched.driverFees && formik.errors.driverFees
-                        ? formik.errors.driverFees
-                        : 'Driver Fees'
-                    }
-                    clearable
-                    fullWidth
-                    size='lg'
-                    placeholder='Stars'
-                    name='driverFees'
-                    id='driverFees'
-                    value={formik.values.driverFees}
-                    onChange={formik.handleChange}
-                    status={
-                      formik.touched.driverFees && formik.errors.driverFees
-                        ? 'error'
-                        : 'default'
-                    }
-                  />
-                </Flex>
+                <Input
+                  label={
+                    formik.touched.driver_fees && formik.errors.driver_fees
+                      ? formik.errors.driver_fees
+                      : 'Driver Fees'
+                  }
+                  clearable
+                  fullWidth
+                  size='lg'
+                  placeholder='Stars'
+                  name='driver_fees'
+                  id='driver_fees'
+                  value={formik.values.driver_fees}
+                  onChange={formik.handleChange}
+                  status={
+                    formik.touched.driver_fees && formik.errors.driver_fees
+                      ? 'error'
+                      : 'default'
+                  }
+                />
               </Flex>
             </Modal.Body>
 
