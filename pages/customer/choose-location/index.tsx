c199c077b5527eaa10d38645d2f58140e712961d
ZineddineBk09 <eino1209@gmail.com>
2024-02-07 20:53:13 +0100

    fix

--- pages/customer/choose-location/index.tsx
+++ pages/customer/choose-location/index.tsx
@@ -1,8 +1,13 @@
 import { NextPage } from 'next'
 import React from 'react'
+import LocationForm from '../../../components/customer/location-form'
 
 const CustomerPage: NextPage = () => {
-  return <div></div>
+  return (
+    <div className='w-screen h-screen'>
+      <LocationForm />
+    </div>
+  )
 }
 
 export default CustomerPage
