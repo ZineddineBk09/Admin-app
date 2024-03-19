e088ef52112f6e69fd5fc6b7e7ac0fe9467d2f97
ZineddineBk09 <eino1209@gmail.com>
2024-03-19 15:25:06 +0100

    fix build errors

--- context/map/index.tsx
+++ context/map/index.tsx
@@ -1,4 +1,4 @@
-import { Order, Status } from '../../interfaces'
+import { Address, Order, Status } from '../../interfaces'
 import { faker } from '@faker-js/faker'
 import React, { useEffect, useState } from 'react'
 
@@ -75,100 +75,9 @@ export const MapContextProvider = ({
     setFilteredOrders(filtered)
   }
 
-  const fetchOrders = () => {
-    const arr = []
-    for (let i = 0; i < 20; i++) {
-      const fakeOrder: Order = {
-        id: faker.number.int({ min: 1000, max: 10000 }).toString(),
-        date: faker.date.past().toLocaleDateString(),
-        value: faker.number.int({ max: 1000, min: 0 }),
-        client: {
-          name: faker.company.name(),
-          id: faker.string.uuid(),
-          image: faker.image.url(),
-          address: faker.location.streetAddress(),
-          phone: faker.phone.number(),
-        },
-        customer: {
-          id: faker.string.uuid(),
-          name: faker.person.firstName(),
-          image: faker.image.avatar(),
-          address: faker.location.streetAddress(),
-          phone: faker.phone.number(),
-        },
-        duration: faker.number.int(),
-        startTime: faker.date.past().getTime(),
-        endTime: faker.date.future().getTime(),
-        distance: faker.number.int(),
-        time: faker.date.past().toLocaleTimeString(),
-        driverId: faker.string.uuid(),
-        driverName: faker.person.firstName(),
-        city: faker.location.city(),
-        status: orderStatus[faker.number.int({ max: 3, min: 0 })].value,
-        delivery_fee: faker.number.int({ max: 100, min: 0 }),
-        location:
-          faker.number.int({ max: 2, min: 1 }) == 1
-            ? {
-                latitude: faker.location.latitude({ max: 22, min: 21 }),
-                longitude: faker.location.longitude({
-                  max: 40,
-                  min: 39,
-                }),
-              }
-            : null,
-        items: Array.from(
-          { length: faker.number.int({ max: 5, min: 1 }) },
-          () => ({
-            id: faker.number.int({ min: 1000, max: 10000 }).toString(),
-            name: faker.commerce.productName(),
-            quantity: faker.number.int({ max: 5, min: 1 }),
-          })
-        ),
-        client_paid: faker.datatype.boolean(),
-        driver_paid: faker.datatype.boolean(),
-        paymentType: ['cash', 'visa', 'mastercard'][
-          faker.number.int({ max: 2, min: 0 })
-        ] as any,
-      }
-      arr.push(fakeOrder)
-    }
-    setOrders(arr)
-    setFilteredOrders(arr)
-    setSelectedOrder(arr[0]?.id)
-  }
+  const fetchOrders = () => {}
 
-  const fetchDrivers = () => {
-    const arr = []
-    for (let i = 0; i < 20; i++) {
-      const fakeDriver: any = {
-        id: faker.number.int({ min: 1000, max: 10000 }),
-        username: faker.person.fullName(),
-        email: faker.internet.email(),
-        team: faker.company.name(),
-        completedTasks: faker.number.int({ max: 100, min: 0 }),
-        inProgressTasks: faker.number.int({ max: 100, min: 0 }),
-        warnings: faker.number.int({ max: 20, min: 0 }),
-        activeHours: faker.number.int({ max: 300, min: 0 }),
-        image: faker.image.avatar(),
-        status: driverStatus[faker.number.int({ max: 2, min: 0 })].value as any,
-        location: {
-          latitude: faker.location.latitude({ max: 22, min: 21 }),
-          longitude: faker.location.longitude({
-            max: 40,
-            min: 39,
-          }),
-        },
-        orders: faker.number.int({
-          max: 10,
-          min: 0,
-        }),
-        phone: faker.phone.number(),
-      }
-      arr.push(fakeDriver)
-    }
-    setDrivers(arr as any[])
-    setSelectedDriver(arr[0]?.id)
-  }
+  const fetchDrivers = () => {}
 
   useEffect(() => {
     fetchOrders()
