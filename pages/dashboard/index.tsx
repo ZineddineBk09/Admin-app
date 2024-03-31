fcda4aa618981036df8c2b867e7e897a6fe3be45
ZineddineBk09 <eino1209@gmail.com>
2024-03-31 15:43:30 +0100

    fix

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,6 +2,7 @@ import Loading from '../../components/shared/loading'
 import type { NextPage } from 'next'
 // import { Content } from '../../components/map/content'
 import dynamic from 'next/dynamic'
+import { MapDriversContextProvider } from '../../context/map/drivers'
 const Content = dynamic(
   () => import('../../components/map/content').then((mod) => mod.Content),
   {
