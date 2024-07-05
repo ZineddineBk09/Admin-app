2d74a6d159a725aef57c0a7e8dd4e6ed37678767
ZineddineBk09 <eino1209@gmail.com>
2024-07-05 15:04:26 +0100

    fix

--- components/table/admin/driver/drivers-table.tsx
+++ components/table/admin/driver/drivers-table.tsx
@@ -1,17 +1,137 @@
-import { Table, useAsyncList, useCollator } from "@nextui-org/react";
+import {
+  Badge,
+  Col,
+  Row,
+  Table,
+  useAsyncList,
+  useCollator,
+  User,
+} from "@nextui-org/react";
 import { useDriversContext } from "../../../../context/admin/drivers";
+import { faker } from "@faker-js/faker";
+import { CancelIcon } from "../../../icons/orders";
+import { IconButton } from "../table.styled";
+import { EditIcon } from "../../../icons/table";
 
 export default function DriverTable() {
+  interface Driver {
+    id: string;
+    first_name: string;
+    last_name: string;
+    device_id: string;
+    phone_number: string;
+    zone: string;
+    nationality: string;
+    id_number: string;
+    id_image: string;
+    driving_license_number: string;
+    driving_license_img: string;
+    pricing: string;
+    status: "pending" | "active" | "suspend";
+  }
+
+  const fakeDrivers: Driver[] = Array.from({ length: 100 }, () => ({
+    id: faker.string.uuid(),
+    first_name: faker.person.firstName(),
+    last_name: faker.person.lastName(),
+    device_id: faker.phone.imei(),
+    phone_number: faker.phone.number(),
+    zone: faker.address.city(),
+    nationality: faker.location.country(),
+    id_number: faker.string.uuid(),
+    id_image: faker.image.avatar(),
+    driving_license_number: faker.string.uuid(),
+    driving_license_img: faker.image.avatar(),
+    pricing: faker.number.int({ min: 100, max: 1000 }).toString(),
+    status: ["pending", "active", "suspend"][
+      faker.number.int({ min: 0, max: 2 })
+    ] as "pending" | "active" | "suspend",
+  }));
+
+  const columns = [
+    {
+      id: "driver",
+      title: "Driver",
+      render: () => {
+        return (
+          <User
+            src={faker.image.avatar()}
+            name={faker.person.firstName() + " " + faker.person.lastName()}
+            description={faker.phone.number()}
+            squared
+          />
+        );
+      },
+    },
+    { id: "device_id", title: "Device ID" },
+    { id: "zone", title: "Zone" },
+    { id: "nationality", title: "Nationality" },
+    { id: "id_number", title: "ID Number" },
+    {
+      id: "driving_license_number",
+      title: "Driving License Number",
+    },
+    {
+      id: "pricing",
+      title: "Pricing",
+      render: (pricing: string) =>
+        new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(parseInt(pricing)),
+    },
+    {
+      id: "status",
+      title: "Status",
+      render: (status: string) => {
+        return (
+          <Badge
+            color={
+              status === "pending"
+                ? "warning"
+                : status === "active"
+                ? "success"
+                : "error"
+            }
+          >
+            {status}
+          </Badge>
+        );
+      },
+    },
+    {
+      id: "actions",
+      title: "Actions",
+      render: () => {
+        return (
+          <Row
+            justify="center"
+            align="center"
+            css={{ gap: "$8", "@md": { gap: 3 } }}
+          >
+            <Col css={{ d: "flex" }}>
+              <IconButton>
+                <EditIcon size={20} fill="#979797" />
+              </IconButton>
+            </Col>
+            <Col css={{ d: "flex" }}>
+              <IconButton>
+                <CancelIcon />
+              </IconButton>
+            </Col>
+          </Row>
+        );
+      },
+    },
+  ];
+
   const collator = useCollator({ numeric: true });
   const { drivers, loading } = useDriversContext();
 
   async function load({ signal }: any) {
-    const res = await fetch("https://swapi.py4e.com/api/people/?search", {
-      signal,
-    });
-    const json = await res.json();
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     return {
-      items: json.results,
+      items: fakeDrivers,
     };
   }
 
@@ -31,23 +151,6 @@ export default function DriverTable() {
 
   const list = useAsyncList({ load, sort });
 
-  /**interface Driver {
-  id: string | number
-  user: User
-  city: City
-  driver_type: DriverType
-  team: DriverTeam
-  code: string
-  phone_number: string
-  status: 'active' | 'inactive' | 'PickingUp' | 'delivering' | 'waiting'
-  image: string
-  vehicle_license: string
-  residency_id: string
-  is_idle: boolean
-} */
-
-  const columns = [{ key: "name", label: "Name" }];
-
   return (
     <Table
       aria-label="drivers table"
@@ -57,23 +160,25 @@ export default function DriverTable() {
       animated
     >
       <Table.Header>
-        <Table.Column key="name" allowsSorting align="center">
-          Name
-        </Table.Column>
-        <Table.Column key="height" allowsSorting align="center">
-          Height
-        </Table.Column>
-        <Table.Column key="mass" allowsSorting align="center">
-          Mass
-        </Table.Column>
-        <Table.Column key="birth_year" allowsSorting align="center">
-          Birth Year
-        </Table.Column>
+        {columns.map((column) => (
+          <Table.Column key={column.id} allowsSorting align="center">
+            {column.title}
+          </Table.Column>
+        ))}
       </Table.Header>
       <Table.Body items={list.items} loadingState={list.loadingState}>
         {(item: any) => (
           <Table.Row key={item.name}>
-            {(columnKey) => <Table.Cell>{item[columnKey]}</Table.Cell>}
+            {(columnKey) => (
+              <Table.Cell>
+                {columns.find((column) => column.id === columnKey)?.render
+                  ? // @ts-ignore
+                    columns
+                      .find((column) => column.id === columnKey)
+                      ?.render(item[columnKey])
+                  : item[columnKey]}
+              </Table.Cell>
+            )}
           </Table.Row>
         )}
       </Table.Body>
@@ -83,6 +188,7 @@ export default function DriverTable() {
         align="center"
         rowsPerPage={10}
         onPageChange={(page) => console.log({ page })}
+        total={list.items.length / 10}
       />
     </Table>
   );
